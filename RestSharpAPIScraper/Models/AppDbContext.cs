7cdc767c1a9c951d4d09a463a250f12b51fc06cb
vjpal3 <vrishalipal@yahoo.com>
2019-12-07 13:29:55 -0500

    Configure DbContext class and connection string

---
+++ RestSharpAPIScraper/Models/AppDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace RestSharpAPIScraper.Models
+{
+    class AppDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(Helper.GetConnectionString("APIScraperDB"));
+        }
+
+        public DbSet<StockData> StocksData { get; set; }
+    }
+}
---
+++ RestSharpAPIScraper/Models/Helper.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestSharpAPIScraper.Models
+{
+    class Helper
+    {
+        public static string GetConnectionString(string name)
+        {
+            return ConfigurationManager.ConnectionStrings[name].ConnectionString;
+        }
+    }
+}
